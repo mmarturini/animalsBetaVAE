b25df84e2c39d4002b1954613341229955097909
mmarturini <m.marturini22@gmail.com>
2022-12-04 17:35:50 +0100

    cleaned repo

--- catVAEb120.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#PBS -q dssc_gpu
-#PBS -l nodes=1:ppn=1
-#PBS -l walltime=12:00:00
-
-cd $PBS_O_WORKDIR
-/u/dssc/mmarturi/.conda/envs/DeepLearning/bin/python3.9 catVAEb120.py > catVAEb120.out
-
-
--- wildVAE/wildVAEb120.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#PBS -q dssc_gpu
-#PBS -l nodes=1:ppn=1
-#PBS -l walltime=10:00:00
-
-cd $PBS_O_WORKDIR
-/u/dssc/mmarturi/.conda/envs/DeepLearning/bin/python3.9 wildVAEb120.py > wildVAEb120.out
-
-
